4c10939a93857e1871396c0315f700f007bebf51
hhddj <1466742963@qq.com>
2020-01-08 14:58:05 +0800

    8 commit

--- jiexi_request.py
+++ jiexi_request.py
@@ -5,9 +5,6 @@ argtest=sys.argv[1] #需要进行测试的参数
 argvalue=sys.argv[2] #参数值，即poc
 txtpath=sys.argv[3] #txt-request路径
 
-#可将poc修改
-#argvalue='['+argvalue[:-1]
-
 with open(str(txtpath), 'r') as f1:
     str = f1.readlines()
 str1=[]
@@ -35,7 +32,7 @@ if method=='POST':
     else:
         for i in arg:
             arg1.append(i.split('=')[0])
-            arg2.append(i.split('=')[1])
+            aarg2.append(i.split('=')[1])
         args = dict(zip(arg1, arg2))
         args[argtest] = argvalue
     host=headers['Host']
@@ -44,7 +41,7 @@ if method=='POST':
     res=requests.post(url, headers=headers,data=args)
     print(res.text)
 elif method=='GET':
-    header = str1[2:]
+    header = str1[1:]
     for i in header:
         header1.append(i.split(': ')[0])
         header2.append(i.split(': ')[1])
@@ -59,8 +56,8 @@ elif method=='GET':
         params[argtest] = argvalue
     except:
         print("参数不存在！使用原数据包发送！")
-    host=params['Host']
-    params.pop('Host')
+    host=headers['Host']
+    headers.pop('Host')
     url = 'http://' + host + path.split('?')[0]
     res=requests.get(url,headers=headers,params=params)
     print(res.text)
